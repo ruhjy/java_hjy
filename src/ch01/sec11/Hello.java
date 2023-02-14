12a7d4cde15d6e32a58357eaff6dccfca58b90b4
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-14 10:55:11 +0900

    first commit

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,21 @@
+package ch01.sec11;
+
+/**
+ * @author 허재엽
+ */
+/*
+ * 장제목 : 1장 자바 시작하기, 작성일 : 2023.02.14
+ */
+
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java!!");
+		// 한줄 주석 단축키는 ctrl + /
+		/*
+		 * 여러줄 주석
+		 * 여러줄 주석
+		 */
+	}
+}
---
+++ src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,16 @@
+package ch01.sec11;
+
+public class MainShortCut {
+	public static void main(String[] args) {
+		System.out.println("Hi, JAVA");
+		System.out.println("출력 되는 내용");
+		System.out.println("출력!!!");
+		
+		// 주석 ctrl + /
+		// 실행 ctrl + F11
+		// 창크기 변경 ctrl + m
+		// 한줄 지우기 ctrl + d
+		// 줄 옮기기 ctrl + 방향키위아래
+		// 힌트얻기 ctrl + space
+	}
+}
