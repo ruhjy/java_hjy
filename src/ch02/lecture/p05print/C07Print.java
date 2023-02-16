2229c217c3414f8aadc1c121bace6c77569405e6
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-16 10:56:40 +0900

    printf - argument_index $

---
+++ src/ch02/lecture/p05print/C07Print.java
@@ -0,0 +1,15 @@
+package ch02.lecture.p05print;
+
+public class C07Print {
+	public static void main(String[] args) {
+		int age = 30;
+		String name = "son";
+		
+		System.out.printf("이름은 %s이고 나이는 %d입니다.%n", name, age);
+		System.out.printf("나이는 %d이고 이름은 %s입니다.%n", age, name);
+		System.out.println();
+		
+		// argument_index $를 쓰면 순번 변경 가능
+		System.out.printf("이름은 %2$s이고 나이는 %1$d입니다.%n", age, name);
+	}
+}
