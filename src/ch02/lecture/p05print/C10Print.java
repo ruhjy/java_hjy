3b561a5622a9b90de477c86d3c31572af7917165
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-16 11:47:16 +0900

    printf - flag, 0, precision

---
+++ src/ch02/lecture/p05print/C10Print.java
@@ -0,0 +1,32 @@
+package ch02.lecture.p05print;
+
+public class C10Print {
+	public static void main(String[] args) {
+		// flag
+		// - : 왼쪽 정렬
+		
+		System.out.printf("%d%n", 33);
+		System.out.printf("%5d%n", 33);
+		System.out.printf("%-5d%n", 33);
+		
+		System.out.printf("%-10s입니다.%n", "son");
+		System.out.println();
+		
+		// 0 : 0으로 공백 채움 (정수, 실수에서 사용가능)
+		System.out.printf("%d%n", 99);
+		System.out.printf("%5d%n", 99);
+		System.out.printf("%05d%n", 99);
+		System.out.printf("%1$05d%n", 99);
+		System.out.println();
+		
+		System.out.printf("%010f%n", 3.14);
+		System.out.println();
+		
+		// precision : 소수점 이하 자릿수
+		System.out.printf("%10.6f%n", 3.14);
+		System.out.printf("%10.3f%n", 3.14);
+		System.out.printf("%10.3f%n", 3.5555);
+		System.out.printf("%010.3f%n", 3.5555);
+		System.out.printf("%1$010.3f%n", 3.5555);
+	}
+}
