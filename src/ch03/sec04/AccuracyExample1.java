51b4a5c47f6c8a0b63f49d0a75538ef47a2a1772
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-16 14:59:45 +0900

    정확한 계산은 정수 연산으로

--- src/ch03/sec04/AccuracyExample1.java
+++ src/ch03/sec04/AccuracyExample1.java
@@ -7,6 +7,6 @@ public class AccuracyExample1 {
 		int number = 7;
 		
 		double result = apple - number * pieceUnit;
-		System.out.println(result);
+		System.out.println("사과 1개에서 남은 양 : " + result);
 	}
 }
