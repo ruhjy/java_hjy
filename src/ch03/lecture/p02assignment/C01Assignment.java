0d18e7bcacc83945fd2a50c3232a2cc7503e9fb1
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-17 09:59:24 +0900

    대입 연산자

---
+++ src/ch03/lecture/p02assignment/C01Assignment.java
@@ -0,0 +1,26 @@
+package ch03.lecture.p02assignment;
+
+public class C01Assignment {
+	public static void main(String[] args) {
+		// 할당(대입) 연산자
+		// =, +=, -=, *=, /=, %=
+		
+		int x = 10;
+		
+		x += 30; // x = x + 30;
+		System.out.println(x); // 40
+		
+		x -= 20; // x = x - 20;
+		System.out.println(x); // 20
+		
+		x *= 3; // x = x * 3;
+		System.out.println(x); // 60
+		
+		x /= 7; // x = x / 7; 
+		System.out.println(x); // 8
+		
+		x %= 3; // x = x % 3;
+		System.out.println(x); // 2
+		
+	}
+}
