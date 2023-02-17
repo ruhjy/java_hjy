a94b69baa0fa3c2a6e93132382114d93039aa985
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-17 09:59:00 +0900

    chapter03 확인문제

---
+++ src/ch03/exercise/Example01.java
@@ -0,0 +1,15 @@
+package ch03.exercise;
+
+public class Example01 {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		
+		int z = (++x) + (y--);
+		
+		System.out.println("z = " + z);
+		
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
