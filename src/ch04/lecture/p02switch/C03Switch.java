4e31eb0a56bcf69d15bf14c1a60f4b01beccde40
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-20 12:37:06 +0900

    switch문

--- src/ch04/lecture/p02switch/C03Switch.java
+++ src/ch04/lecture/p02switch/C03Switch.java
@@ -3,7 +3,7 @@ package ch04.lecture.p02switch;
 public class C03Switch {
 	public static void main(String[] args) {
 		// switch에서 사용 가능한 값 type
-		// byte, short, int, long, char, String, enum, Character, Byte, Short, Integer
+		// byte, short, int, char, String, enum, Character, Byte, Short, Integer
 		// long 타입은 컴파일 에러
 		// Cannot switch on a value of type long. Only convertible int values, strings or enum variables are permitted
 		
