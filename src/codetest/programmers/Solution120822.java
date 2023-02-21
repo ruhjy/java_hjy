222b6ac875db87b747aef2e24884285c6050bc70
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-21 11:30:09 +0900

    프로그래머스 문제

--- src/codetest/programmers/Solution120822.java
+++ src/codetest/programmers/Solution120822.java
@@ -3,22 +3,28 @@ package codetest.programmers;
 public class Solution120822 {
 
 	public static void main(String[] args) {
-		// 01234 
+		// 01234
 //		  "jaron"
-		
+
 		System.out.println(Solution120822.solution("jaron"));
 
 	}
 
 	public static String solution(String my_string) {
 		String answer = "";
-		
-		String[] arr = my_string.split("");
-		
-		for(int i = 1; i<=arr.length; i++) {
-			answer += arr[arr.length-i]; 
+
+//		String[] arr = my_string.split("");
+//		
+//		for(int i = 1; i<=arr.length; i++) {
+//			answer += arr[arr.length-i]; 
+//		}
+//		
+//		return answer;
+
+		for (int i = my_string.length() - 1; i >= 0; i--) {
+			answer += my_string.charAt(i);
 		}
-		
+
 		return answer;
 	}
 }
