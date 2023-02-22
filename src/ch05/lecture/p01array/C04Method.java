d81f98c9e828f0439e760fe6eb24729a2f03e922
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-22 11:10:30 +0900

    array 배열

---
+++ src/ch05/lecture/p01array/C04Method.java
@@ -0,0 +1,20 @@
+package ch05.lecture.p01array;
+
+public class C04Method {
+
+	public static void main(String[] args) {
+		methodName(3, 4); // 메서드 호출(실행)
+		
+		methodName2(new int[] { 3, 4, 5 });
+	}
+
+	public static void methodName2(int[] arr) {
+
+	}
+
+	// method
+	public static void methodName(int param1, int param2) {
+
+	}
+
+}
