517a8e3edf2c336b518722278650ccd9e3d0b95e
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-22 12:41:08 +0900

    참조 타입

---
+++ src/ch05/lecture/p02reference/C02Reference.java
@@ -0,0 +1,32 @@
+package ch05.lecture.p02reference;
+
+public class C02Reference {
+	public static void main(String[] args) {
+
+		int a = 3;
+		int b = a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println();
+
+		a = 6;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println();
+		
+		int[] arr1 = { 5, 6, 7 };
+		int[] arr2 = arr1;
+		
+		System.out.println(arr1[0]);
+		System.out.println(arr2[0]);
+		System.out.println();
+		
+		arr1[0] = 99;
+		
+		System.out.println(arr1[0]);
+		System.out.println(arr2[0]);
+		
+	}
+}
