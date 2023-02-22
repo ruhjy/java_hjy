eccaee187236a3df2f49f8531dc3432670d0693c
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-22 21:48:52 +0900

    프로그래머스 문제

--- src/codetest/programmers/Solution120813.java
+++ src/codetest/programmers/Solution120813.java
@@ -10,29 +10,36 @@ public class Solution120813 {
 //		System.out.println(n1);
 
 		Solution10 s = new Solution10();
-		System.out.println(Arrays.toString(s.solution(10)));
+		System.out.println(Arrays.toString(s.solution(5)));
 	}
 }
 
 class Solution10 {
 	public int[] solution(int n) {
-		int num = 0;
-		if (n % 2 == 0) {
-			num = n / 2;
-		} else {
-			num = (n / 2) + 1;
-		}
 
-		int[] answer = new int[num];
-		int i = 0;
-
-		for (int j = 1; j <= n; j++) {
-			if (j % 2 != 0) {
-				answer[i] += j;
-				i++;
-			}
-		}
+		int[] answer = IntStream.rangeClosed(1, n)
+				.filter(i -> i % 2 == 1)
+				.toArray();
 		return answer;
+
+//		int num = 0;
+//		if (n % 2 == 0) {
+//			num = n / 2;
+//		} else {
+//			num = (n / 2) + 1;
+//		}
+//
+//		int[] answer = new int[num];
+//		int i = 0;
+//
+//		for (int j = 1; j <= n; j++) {
+//			if (j % 2 != 0) {
+//				answer[i] += j;
+//				i++;
+//			}
+//		}
+//		return answer;
+
 //      배열을 스트림으로 변환.중복제거.배열로 리턴;
 //		return Arrays.stream(answer).distinct().toArray();
 
