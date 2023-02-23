e6da6ef7f858078c8d89c1593444212c1b7d797b
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-23 14:44:46 +0900

    백준 문제

--- src/codetest/baekjoon/Problem1152.java
+++ src/codetest/baekjoon/Problem1152.java
@@ -1,18 +1,28 @@
 package codetest.baekjoon;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Problem1152 {
 	public static void main(String[] args) {
 //		String input = "The Curious Case of Benjamin Button"; // 6
-//		String input = "The first character is a blank"; // 6
-//		String input = "The last character is a blank"; // 6
-		String input = "How are you? Good."; // 4
+//		String input = " The first character is a blank"; // 6
+		String input = "The last character is a blank "; // 6
+//		String input = "How are you? Good."; // 4
 
 		Scanner scan = new Scanner(input);
 
-		String[] strArr1 = scan.nextLine().split(" ");
-		System.out.println(strArr1.length);
+//		String[] strArr1 = scan.nextLine().split(" ");
+//		System.out.println(strArr1.length);
+
+		String line = scan.nextLine();
+		String[] arr = line.split(" ");
+		int answer = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (!arr[i].equals("")) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
 	}
 }
