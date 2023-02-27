5c9b20752d5d60d3b406c18222546be78d6d4f6c
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-27 14:59:31 +0900

    메서드

---
+++ src/ch06/lecture/p03method/MyClass01.java
@@ -0,0 +1,27 @@
+package ch06.lecture.p03method;
+
+public class MyClass01 {
+	// field
+
+	// method
+	// 리턴타입 메서드명(파라미터 목록) {
+	//     실행문들...}
+	// }
+	// 메서드명 작성 관습 : lowerCamelCase
+	void methodName(int param) {
+		// 실행문들...
+
+	}
+
+	void run() {
+
+	}
+	
+	void wakeUp() {
+		
+	}
+	
+	void standUp() {
+		
+	}
+}
