b72d182c3f7da82bc37168c530a17202d7315433
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-27 10:27:56 +0900

    정규 표현식

---
+++ src/ch05/lecture/p08regex/C25RegEx.java
@@ -0,0 +1,24 @@
+package ch05.lecture.p08regex;
+
+public class C25RegEx {
+	public static void main(String[] args) {
+		// unicode script
+		// \p{IsHangul}
+		// 한글 [가-힣]
+
+		String pattern1 = "[가-힣]";
+		String pattern2 = "\\p{IsHangul}"; // \p{IsHangul}
+
+		System.out.println("백".matches(pattern1)); // true
+		System.out.println("두".matches(pattern2)); // true
+		System.out.println("산".matches(pattern2)); // true
+		System.out.println();
+
+		// 공백없이 한글2글자 이상
+		String pattern3 = "\\p{IsHangul}{2,}";
+		System.out.println("두산".matches(pattern3)); // true
+		System.out.println("백두산".matches(pattern3)); // true
+		System.out.println("산".matches(pattern3)); // false
+
+	}
+}
