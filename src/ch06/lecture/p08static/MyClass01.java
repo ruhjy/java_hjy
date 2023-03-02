f71cd94b2f14231e0ea66f35aaa9de55819bb9ac
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-02 11:53:46 +0900

    static (정적) 블럭

---
+++ src/ch06/lecture/p08static/MyClass01.java
@@ -0,0 +1,23 @@
+package ch06.lecture.p08static;
+
+public class MyClass01 {
+	// 필드
+
+	// 인스턴스 필드
+	String name;
+
+	// 정적(static) 필드
+	static String company;
+
+	// 생성자 : (주로) 인스턴스 필드 초기화
+	MyClass01() {
+		name = "son";
+	}
+
+	// static initialization block - 정적 초기화 블럭
+	// static block : (주로) static 필드 초기화
+	static {
+		// 명령문들
+		company = "sansung";
+	}
+}
