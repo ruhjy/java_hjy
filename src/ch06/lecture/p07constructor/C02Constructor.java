7d430368658c9ebf696e52265560cf5cb5b2794b
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-02 09:37:43 +0900

    생성자 - Constructor

---
+++ src/ch06/lecture/p07constructor/C02Constructor.java
@@ -0,0 +1,12 @@
+package ch06.lecture.p07constructor;
+
+public class C02Constructor {
+	public static void main(String[] args) {
+		
+		// 처음해봄 생성자에 출력문
+		MyClass02 o1 = new MyClass02();
+		MyClass02 o2 = new MyClass02();
+		
+		
+	}
+}
---
+++ src/ch06/lecture/p07constructor/C03Constructor.java
@@ -0,0 +1,22 @@
+package ch06.lecture.p07constructor;
+
+public class C03Constructor {
+	public static void main(String[] args) {
+
+		MyClass03 o1 = new MyClass03();
+		o1.name = "park";
+		o1.age = 66;
+
+		MyClass03 o2 = new MyClass03();
+		o2.name = "cha";
+		o2.age = 55;
+
+		MyClass03 o3 = new MyClass03();
+
+		o1.descript();
+		o2.descript();
+		o3.descript();
+
+	}
+
+}
