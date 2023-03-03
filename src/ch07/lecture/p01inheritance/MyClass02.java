01ffdd70f482abf32944d376caf88143b87d27ce
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-03 16:13:47 +0900

    클래스 상속

---
+++ src/ch07/lecture/p01inheritance/MyClass02.java
@@ -0,0 +1,9 @@
+package ch07.lecture.p01inheritance;
+
+// MyClass01의 멤버(필드, 메서드)를 MyClass02가 상속받는다.
+
+// 자식클래스(Sub class, 하위클래스) : 상속 받은 클래스
+// 부모클래스(Super class, 상위클래스) : 상속해주는 클래스
+public class MyClass02 extends MyClass01 {
+
+}
