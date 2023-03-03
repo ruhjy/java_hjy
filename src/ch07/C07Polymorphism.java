70d1015976a1d07076e432c856b34c526439caf3
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-03 17:16:56 +0900

    자동 타입 변환 (자동 형변환)

---
+++ src/ch07/C07Polymorphism.java
@@ -0,0 +1,48 @@
+package ch07;
+
+public class C07Polymorphism {
+	public static void main(String[] args) {
+
+		Animal o1 = new Animal();
+		Horse o2 = new Horse();
+		Fish o3 = new Fish();
+
+		o1.breath();
+		o2.breath();
+		o3.breath();
+		System.out.println();
+
+		// 실제 인스턴스에 있는 기능으로 동작
+		Animal o4 = new Horse();
+		Animal o5 = new Fish();
+
+		o4.breath();
+		o5.breath();
+
+		Fish f1 = (Fish) o5;
+		f1.breath();
+	}
+}
+
+class Animal {
+
+	public void breath() {
+		System.out.println("호흡한다.");
+	}
+}
+
+class Horse extends Animal {
+
+	@Override
+	public void breath() {
+		System.out.println("폐로 호흡한다.");
+	}
+}
+
+class Fish extends Animal {
+
+	@Override
+	public void breath() {
+		System.out.println("아가미로 호흡한다.");
+	}
+}
---
+++ src/ch07/lecture/p01inheritance/SuperSuper05.java
@@ -0,0 +1,8 @@
+package ch07.lecture.p01inheritance;
+
+public class SuperSuper05 {
+
+	public void method01() {
+		System.out.println("super super method1");
+	}
+}
