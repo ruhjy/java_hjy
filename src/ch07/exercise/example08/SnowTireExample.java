6a43df5913f01270fc9809c5b5c9c4abc6c3651c
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-06 11:12:54 +0900

    chapter07 확인문제

--- src/ch07/exercise/example08/SnowTireExample.java
+++ src/ch07/exercise/example08/SnowTireExample.java
@@ -4,9 +4,9 @@ public class SnowTireExample {
 	public static void main(String[] args) {
 
 		SnowTire snowTire = new SnowTire();
-		Tire tire = new Tire();
+		Tire tire = snowTire;
 
 		snowTire.run(); // 스노우 타이어가 굴러갑니다.
-		tire.run(); // 일반 타이어가 굴러갑니다.
+		tire.run(); // 스노우 타이어가 굴러갑니다.
 	}
 }
