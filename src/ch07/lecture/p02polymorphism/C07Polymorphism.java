9be490c455db41df2bc99d540f6d171bca1cf993
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-06 11:24:08 +0900

    다형성 - 반환타입이 참조타입

---
+++ src/ch07/lecture/p02polymorphism/C07Polymorphism.java
@@ -0,0 +1,31 @@
+package ch07.lecture.p02polymorphism;
+
+public class C07Polymorphism {
+	public static void main(String[] args) {
+
+		Hero hero = new Hero();
+
+		// ... 무기선택
+		Weapon w = getWeapon(1);
+
+		hero.weapon = w;
+		hero.button1();
+	}
+
+	public static Weapon getWeapon(int choice) {
+		switch (choice) {
+			case 1 -> {
+				return new Gun();
+			}
+			case 2 -> {
+				return new Bow();
+			}
+			case 3 -> {
+				return new Sword();
+			}
+			default -> {
+				return new Weapon();
+			}
+		}
+	}
+}
