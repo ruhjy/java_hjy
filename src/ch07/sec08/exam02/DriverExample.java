ae0762a7f4b4819c997c8f58c5ac9f35ede86723
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-06 11:00:49 +0900

    매개변수의 다형성

---
+++ src/ch07/sec08/exam02/DriverExample.java
@@ -0,0 +1,17 @@
+package ch07.sec08.exam02;
+
+public class DriverExample {
+	public static void main(String[] args) {
+
+		// Driver 객체 생성
+		Driver driver = new Driver();
+
+		// 매개값으로 Bus 객체를 제공하고 drive() 메서드 호출
+		Bus bus = new Bus();
+		driver.drive(bus); // driver.drive(new Bus()); 동일
+
+		// 매개값으로 Taxi 객체를 제공하고 drive() 메서드 호출
+		Taxi taxi = new Taxi();
+		driver.drive(taxi); // driver.drive(new Taxi()); 동일
+	}
+}
