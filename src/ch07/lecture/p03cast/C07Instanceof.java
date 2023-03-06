cd249e9c84f0edd6db85a5cc099b8772440b5f06
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-06 15:15:42 +0900

    instanceof 연산자

---
+++ src/ch07/lecture/p03cast/C07Instanceof.java
@@ -0,0 +1,30 @@
+package ch07.lecture.p03cast;
+
+public class C07Instanceof {
+	public static void main(String[] args) {
+
+		action(new Animal());
+		System.out.println();
+
+		action(new Horse());
+		System.out.println();
+
+		action(new Fish());
+	}
+
+	public static void action(Animal a) {
+		a.breath();
+
+		if (a instanceof Horse) {
+			// 말이면 run() 실행
+			Horse h = (Horse) a;
+			h.run();
+		} else if (a instanceof Fish) {
+			// 물고기면 swim() 실행
+			Fish f = (Fish) a;
+			f.swim();
+		} else {
+			System.out.println("말도 아니고 물고기도 아닙니다.");
+		}
+	}
+}
