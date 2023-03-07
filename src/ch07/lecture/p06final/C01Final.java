581a3b43ca45922436fbcb5da0268f133723d248
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-07 09:47:43 +0900

    final 클래스와 메서드

---
+++ src/ch07/lecture/p06final/C01Final.java
@@ -0,0 +1,12 @@
+package ch07.lecture.p06final;
+
+public class C01Final {
+
+}
+
+// final 클래스
+// 이 클래스를 상속할 수 없음
+final class Super01 {
+}
+
+//class Sub01 extends Super01 { }// The type Sub01 cannot subclass the final class Super01
