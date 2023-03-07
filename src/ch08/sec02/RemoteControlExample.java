dcbfd37d722c7f62c119908dd00732f9657a2a70
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-07 14:49:23 +0900

    인터페이스

--- src/ch08/sec02/RemoteControlExample.java
+++ src/ch08/sec02/RemoteControlExample.java
@@ -8,7 +8,6 @@ public class RemoteControlExample {
 		// rc 변수에 Television 객체를 대입
 		rc = new Television();
 		rc.turnOn();
-		System.out.println();
 
 		// rc 변수에 Audio 객체를 대입(교체시킴)
 		rc = new Audio();
