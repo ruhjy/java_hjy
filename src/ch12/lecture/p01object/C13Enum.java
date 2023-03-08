db2f2c0473084cacc018f518468dbc5a4935135c
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-08 16:41:11 +0900

    열거 타입 (Enum)

---
+++ src/ch12/lecture/p01object/C13Enum.java
@@ -0,0 +1,17 @@
+package ch12.lecture.p01object;
+
+public class C13Enum {
+	public static void main(String[] args) {
+
+		Season season = Season.FALL;
+
+		String a = switch (season) {
+			case SPRING, FALL -> "좋은 계절이다.";
+			case SUMMER -> "덥다";
+			case WINTER -> "춥다";
+//			default -> "잘못입력"; // 생략 가능 - enum의 개수가 정해져 있어서인가
+		};
+
+		System.out.println(a);
+	}
+}
