62c0afb9373ca7aebf001733e928cf02ad7e17b2
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-08 10:38:56 +0900

    인터페이스 - private static 메서드

---
+++ src/ch08/lecture/p04etc/C05PrivateStaticMethod.java
@@ -0,0 +1,10 @@
+package ch08.lecture.p04etc;
+
+public class C05PrivateStaticMethod {
+	public static void main(String[] args) {
+
+		MyInterface05.method1();
+		MyInterface05.method2();
+//		MyInterface05.common(); // private static method
+	}
+}
