3f43b4a609ddc23f95e14a38b89c3760b6935754
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-08 10:28:16 +0900

    인터페이스 - 상수 필드

---
+++ src/ch08/lecture/p04etc/MyInterface03.java
@@ -0,0 +1,12 @@
+package ch08.lecture.p04etc;
+
+public interface MyInterface03 {
+
+	// static field
+	public static final int I = 3;
+
+	// 항상 public static final - 생략 가능
+	int J = 5;
+	String MY_HOME = "seoul";
+
+}
