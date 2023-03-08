04f4617417c77363dba316fb4431abed03a298ef
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-08 10:38:44 +0900

    인터페이스 - static 메서드

---
+++ src/ch08/lecture/p04etc/MyInterface04.java
@@ -0,0 +1,18 @@
+package ch08.lecture.p04etc;
+
+public interface MyInterface04 {
+
+	// public static final field
+
+	// public static method
+	static void method1() {
+		System.out.println("스태틱 메서드");
+	}
+
+	// public abstract instance method
+
+	// public default instance method
+
+	// private instance method
+
+}
