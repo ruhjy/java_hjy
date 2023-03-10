4091980345cfa149e7166700fa48f0f9b4f521dd
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-10 15:19:53 +0900

    chapter09 확인문제

---
+++ src/ch09/exercise/example04/Car.java
@@ -0,0 +1,9 @@
+package ch09.exercise.example04;
+
+public class Car {
+	class Tire {
+	}
+
+	static class Engine {
+	}
+}
---
+++ src/ch09/exercise/example04/CarExample.java
@@ -0,0 +1,12 @@
+package ch09.exercise.example04;
+
+public class CarExample {
+	public static void main(String[] args) {
+
+		Car car = new Car();
+
+		Car.Tire tire = car.new Tire();
+
+		Car.Engine engine = new Car.Engine();
+	}
+}
