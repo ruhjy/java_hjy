ad3f75d92a30449a6e67df126fe7e4c51a571057
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-10 16:48:14 +0900

    익명 클래스

---
+++ src/ch09/lecture/p02anonymous/C04Anonymous.java
@@ -0,0 +1,21 @@
+package ch09.lecture.p02anonymous;
+
+public class C04Anonymous {
+	public static void main(String[] args) {
+
+		class SubClass04 extends MyClass04 {
+			@Override
+			void method1() {
+				System.out.println("재정의한 메서드");
+			}
+		}
+
+		MyClass04 o1 = new SubClass04();
+		o1.method1();
+	}
+
+}
+
+abstract class MyClass04 {
+	abstract void method1();
+}
