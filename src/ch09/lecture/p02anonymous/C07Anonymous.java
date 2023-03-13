f7843124d9963e82a5e90ee076d948095588f049
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-13 10:42:30 +0900

    익명 구현 객체 - 인터페이스 활용

---
+++ src/ch09/lecture/p02anonymous/C07Anonymous.java
@@ -0,0 +1,23 @@
+package ch09.lecture.p02anonymous;
+
+public class C07Anonymous {
+	public static void main(String[] args) {
+
+		MyInterface07 o1 = new MyClass07();
+		
+		o1.method1();
+
+	}
+}
+
+class MyClass07 implements MyInterface07 {
+	
+	@Override
+	public void method1() {
+		System.out.println("재정의된 메서드");
+	}
+}
+
+interface MyInterface07 {
+	void method1();
+}
---
+++ src/ch09/sec07/exam02/RemoteControl.java
@@ -0,0 +1,10 @@
+package ch09.sec07.exam02;
+
+public interface RemoteControl {
+	
+	// 추상 메서드
+	void turnOn();
+	
+	void turnOff();
+	
+}
