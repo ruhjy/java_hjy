d947a928257417b7ae22718e8472ec7775db0917
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-14 16:10:22 +0900

    chapter5 6번문제 list로 풀기

---
+++ src/ch15/exercise/example0506/Example0506.java
@@ -0,0 +1,31 @@
+package ch15.exercise.example0506;
+
+import java.util.*;
+
+public class Example0506 {
+	public static void main(String[] args) {
+
+		int[][] array = {
+				{ 95, 86 },
+				{ 83, 92, 96 },
+				{ 78, 83, 93, 87, 88 }
+		};
+		System.out.println(array.length); // 3
+		System.out.println(array[2].length); // 5
+		System.out.println("--------------------------");
+
+		List<Integer> list1 = List.of(95, 86);
+		List<Integer> list2 = List.of(83, 92, 96);
+		List<Integer> list3 = List.of(78, 83, 93, 87, 88);
+
+		List<List<Integer>> listk = new ArrayList<>();
+		listk.add(list1);
+		listk.add(list2);
+		listk.add(list3);
+		System.out.println(listk.size());
+		System.out.println(list3.size());
+
+		System.out.println(listk);
+
+	}
+}
