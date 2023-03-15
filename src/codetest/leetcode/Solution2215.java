59956e8189eabd93e9ef1f488a41a29e4009d0af
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-15 15:59:50 +0900

    leetcode 문제

---
+++ src/codetest/leetcode/Solution2215.java
@@ -0,0 +1,62 @@
+package codetest.leetcode;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Solution2215 {
+
+	public static void main(String[] args) {
+		int[] arr1 = { 1, 2, 3 };
+		int[] arr2 = { 2, 4, 6 };
+
+//		Set<Integer> set1 = new HashSet<>();
+//		set1.add(1);
+//		set1.add(2);
+//		set1.add(3);
+//		Set<Integer> set2 = new HashSet<>();
+//		set2.add(2);
+//		set2.add(4);
+//		set2.add(6);
+//
+//		List<Integer> list1 = new ArrayList<>();
+//		List<Integer> list2 = new ArrayList<>();
+//
+//		list1.addAll(set1);
+//		list2.addAll(set2);
+//
+//		list1.removeIf(set2::contains);
+//		list1.removeIf((int i) -> set2.contains(i));
+//		list1.removeIf((Integer i) -> {
+//			set2.contains(i));	
+//		};
+
+//		Iterator<Integer> iter = set1.iterator();
+//		while (iter.hasNext()) {
+//			Integer tmp = iter.next();
+//			if (!set2.contains(tmp)) {
+//				set3.add(tmp);
+//			}
+//		}
+//		System.out.println(set3);
+//
+//		Iterator<Integer> iter2 = set2.iterator();
+//		while (iter2.hasNext()) {
+//			Integer tmp = iter2.next();
+//			if (!set1.contains(tmp))
+//				set4.add(tmp);
+//		}
+//		System.out.println(set4);
+//		List<List<Integer>> list1 = new ArrayList<>();
+//		List<Integer> list2 = new ArrayList<>(set3);
+//		List<Integer> list3 = new ArrayList<>(set4);
+//		list1.add(list2);
+//		list1.add(list3);
+//		System.out.println(list1);
+
+	}
+
+//	public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+//		// 코드 작성
+//
+//	}
+}
