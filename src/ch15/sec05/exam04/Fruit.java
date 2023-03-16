4dcfa17d96df445e9899479bb6df498436898736
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-16 16:00:12 +0900

    Comparator

---
+++ src/ch15/sec05/exam04/Fruit.java
@@ -0,0 +1,31 @@
+package ch15.sec05.exam04;
+
+import java.util.*;
+
+class Fruit {
+
+	public String name;
+	public int price;
+
+	public Fruit(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
