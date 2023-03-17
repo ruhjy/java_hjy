a802d4c453d0db3e3666f99edcbb3c8396c3bfdd
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-17 17:17:06 +0900

    최종연산

--- src/ch17/lecture/p02terminal/C08MaxMin.java
+++ src/ch17/lecture/p02terminal/C08MaxMin.java
@@ -7,20 +7,20 @@ public class C08MaxMin {
 	public static void main(String[] args) {
 
 		int[] arr = { 3, 1, 9, 0, 2, 4, 5 };
-		IntStream stream = Arrays.stream(arr);
-//		int max = Arrays.stream(arr)
-//				.max()
-//				.getAsInt();
-//		.get();
+
+//		IntStream stream = Arrays.stream(arr);
+		int max = Arrays.stream(arr)
+				.max()
+				.getAsInt();
 		System.out.println("max : " + max);
 
 		int min = Arrays.stream(arr)
 				.min()
 				.getAsInt();
 		System.out.println("min : " + min);
-		
+
 		IntStream.of(8, 1, 2)
-			.max()
-			.getAsInt();
+				.max()
+				.getAsInt();
 	}
 }
