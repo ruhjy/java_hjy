8a905d9668ad43c5a67323cbfee92b32fdfc64bc
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-20 11:54:53 +0900

    chapter17 확인문제

---
+++ src/ch17/exercies/example05/Example.java
@@ -0,0 +1,31 @@
+package ch17.exercies.example05;
+
+import java.util.*;
+import java.util.function.*;
+
+public class Example {
+	public static void main(String[] args) {
+
+		List<String> list = Arrays.asList(
+				"This is a java book",
+				"Lambda Expressions",
+				"Java8 supports lambda expressions");
+
+		for (String s : list) {
+			if (s.toLowerCase().contains("java")) {
+				System.out.println(s);
+			}
+		}
+		System.out.println();
+
+		list.stream()
+				.filter(Example::havingJava)
+				.forEach(i -> System.out.println(i));
+
+	}
+
+	public static boolean havingJava(String s) {
+		return s.toLowerCase().contains("java");
+	}
+
+}
