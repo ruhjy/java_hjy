feaae00229f220ceef5b78ec6f87833b430d9faf
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-21 15:21:45 +0900

    chapter11 확인문제

---
+++ src/ch11/lecture/exercise/example05/Example05.java
@@ -0,0 +1,40 @@
+package ch11.lecture.exercise.example05;
+
+public class Example05 {
+
+	public static void m1() {
+		try {
+			method1();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public static void m2() throws Exception {
+		method1();
+	}
+
+	public static void m3() {
+		try {
+			method1();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void m4() {
+		try {
+			method1();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void method1() throws NumberFormatException, ClassNotFoundException {
+
+	}
+}
