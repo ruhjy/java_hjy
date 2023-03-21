e3909975a9b1f95267ba03e8fdd319f22acc2d31
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-21 11:00:10 +0900

    예외 - printStackTrace()

---
+++ src/ch11/lecture/p02try_catch/C04PrintStackTrace.java
@@ -0,0 +1,21 @@
+package ch11.lecture.p02try_catch;
+
+public class C04PrintStackTrace {
+	public static void main(String[] args) {
+
+		// unchecked exception
+		try {
+			String a = "java";
+			System.out.println(a.charAt(4)); // unchecked exception
+		} catch (StringIndexOutOfBoundsException e) {
+			e.printStackTrace();
+		}
+		
+		// checked exception
+		try {
+			Class.forName("java.lang.String"); // checked exception
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
