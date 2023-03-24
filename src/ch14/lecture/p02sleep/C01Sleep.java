50248d47a37462c0c4565c74e36de8c3a1b4c94d
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-24 12:49:03 +0900

    스레드 메서드 사용

---
+++ src/ch14/lecture/p02sleep/C01Sleep.java
@@ -0,0 +1,16 @@
+package ch14.lecture.p02sleep;
+
+public class C01Sleep {
+	public static void main(String[] args) {
+
+		System.out.println("메인쓰레드 시작");
+
+		try {
+			Thread.sleep(2000); // 2초 일시정지
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("메인쓰레드 종료");
+	}
+}
