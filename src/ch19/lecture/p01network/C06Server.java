a236736dd697046abd0d8b78b8995248a004c1fa
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-24 16:32:45 +0900

    네트워크

---
+++ src/ch19/lecture/p01network/C06Server.java
@@ -0,0 +1,43 @@
+package ch19.lecture.p01network;
+
+import java.io.*;
+import java.net.*;
+
+public class C06Server {
+	public static void main(String[] args) {
+
+		int port = 50500;
+
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
+			while (true) {
+				System.out.println("연결 기다리는 중...");
+				Socket socket = serverSocket.accept();
+				Thread t = new Thread(() -> {
+					try (
+							InputStream is = socket.getInputStream();
+							BufferedInputStream bis = new BufferedInputStream(is);
+							InputStreamReader isr = new InputStreamReader(bis)) {
+
+						char[] data = new char[1024];
+						int len = 0;
+
+						System.out.println("입력 받는 중...");
+
+						while ((len = isr.read(data)) != -1) {
+							String input = new String(data, 0, len);
+							System.out.println(input);
+						}
+						socket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
+				t.start();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("프로그램 종료");
+		}
+	}
+}
